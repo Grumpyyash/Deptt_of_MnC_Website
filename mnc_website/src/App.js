2da465ea279680a66c5853a4fe29620a03e3a384
Akshat Tripathi <akshatofficial2019@gmail.com>
2022-10-20 18:38:29 +0530

    added faculty page

--- mnc_website/src/App.js
+++ mnc_website/src/App.js
@@ -4,12 +4,14 @@ import { BrowserRouter as Router, Link, Route, Routes } from "react-router-dom";
 import Home from "./Pages/Home";
 import NotFound from "./Pages/Not-Found";
 import Navbar from "./components/navbar";
+import Faculty from "./Pages/Faculty";
 function App() {
   return (
     <Router>
       <Navbar />
       <Routes>
         <Route exact path="/" element={<Home />} />
+        <Route exact path="/faculty" element={<Faculty />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
