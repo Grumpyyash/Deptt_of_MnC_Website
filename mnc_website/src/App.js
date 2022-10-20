0ae92a58d30791d741b0f1934aa8417f9b5b1e74
Akshat Tripathi <akshatofficial2019@gmail.com>
2022-10-20 15:37:56 +0530

    intitial structure implemented
    Signed-off-by: Akshat Tripathi <akshatofficial2019@gmail.com>

--- mnc_website/src/App.js
+++ mnc_website/src/App.js
@@ -1,11 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter as Router, Link, Route, Routes } from "react-router-dom";
+import Home from "./Pages/Home";
+import NotFound from "./Pages/Not-Found";
+import Navbar from "./components/navbar";
 function App() {
   return (
-    <div className="App">
-      <h1>Department Of Mathematics And Computing</h1>
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ mnc_website/src/Pages/Home/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+
+const Home = () => {
+  return (
+    <div>
+      <h2>This is home page of MNC website.</h2>
+    </div>
+  );
+};
+
+export default Home;
