02bf2dbbca63c318960b0b2fd34cc4394bae16ab
Saurabh Tiwari <thesdtiwari@example.com>
2022-10-22 14:57:24 +0530

    Full form of MnC
    Signed-off-by: Saurabh Tiwari <thesdtiwari@example.com>

--- mnc_website/src/components/navbar/index.jsx
+++ mnc_website/src/components/navbar/index.jsx
@@ -4,7 +4,7 @@ import styles from './style.module.css';
 const Navbar = () => {
   return (
     <div className={styles.navbar_container}>
-      <h1>This is navbar of MNC website.</h1>
+      <h1>This is navbar of Mathematics and Computing website.</h1>
     </div>
   );
 };
